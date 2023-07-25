b71b039013647f9f7294c0e11f0c45e27fb17b5a
Daizaikun <laiglesias.min@gmail.com>
2023-07-25 16:07:23 -0500

    create new resolve

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,17 +1,27 @@
 import { deleteUser } from "./services/deleteUser";
 import { getSavedUsers } from "./services/getSavedUsers";
 import { getUser } from "./services/getUser";
+import { isSaveUser } from "./services/isSaveUser";
 import { saveUser } from "./services/saveUser";
 import { searchUser } from "./services/searchUser";
+import { userExtended } from "./services/userExtended";
 
 export const resolvers = {
-  Query: {
-    searchUser,
-    getUser,
-    getSavedUsers,
-  },
-  Mutation: {
-    saveUser,
-    deleteUser
-  },
+	UserSearchResult: {
+		extended: userExtended,
+	},
+
+	User: {
+		save: isSaveUser,
+	},
+
+	Query: {
+		searchUser,
+		getUser,
+		getSavedUsers,
+	},
+	Mutation: {
+		saveUser,
+		deleteUser,
+	},
 };
